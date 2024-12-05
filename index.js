28fd9b50682bfa65cc82bb85bd8f4cc31128382d
Bernardino Alvarado Hernández <147689176+BERCHNARD10@users.noreply.github.com>
2024-12-05 15:46:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const cors = require('cors');
 const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Permite el uso de cookies si es necesario
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
